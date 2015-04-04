163615b70f6494b5d6defc763133860cff703439
Rui Gil <ruipgil@gmail.com>
2015-04-04 22:51:34 +0100

    Added input for age

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Motivation
 Motivation landing page for chrome
+
+This is inspired by [Motivation](https://chrome.google.com/webstore/detail/motivation/ofdgfpchbidcgncgfpdlpclnpaemakoj?hl=en)
--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 var MS_PER_YEAR = 31556900000;
-var byear, bmonth, bday;
-var birth = new Date(byear, bmonth, bday);
 
-function update() {
+function showAge(birth) {
 	var now = new Date();
 	var diff = ('' + ((now - birth)/MS_PER_YEAR)).split('.');
 	var year = diff[0];
 	var mili = diff[1];
 
+	document.querySelector('.age').style.display = 'block';
+	document.querySelector('.question').style.display = 'none';
+
 	document.querySelector('.data-year').innerText = year;
 	document.querySelector('.data-mili').innerText = mili;
 }
 
+var load;
+
+function askAge() {
+	document.querySelector('.age').style.display = 'none';
+	document.querySelector('.question').style.display = 'block';
+
+	document.querySelector('.question #ok').addEventListener('click', function() {
+		var value = document.querySelector('.question #date-input').value.split("/").map(function(elm){ return Number(elm); });
+		localStorage.setItem('birthdate', new Date(value[2], value[1]-1, value[0]).getTime());
+		load();
+	})
+}
+
+load = function () {
+	if(localStorage.getItem('birthdate')) {
+		showAge(new Date(Number(localStorage.getItem('birthdate'))));
+	} else {
+		askAge();
+	}
+}
+
 window.addEventListener('load', function() {
-	update();
+	load();
 });
